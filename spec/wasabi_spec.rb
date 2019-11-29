4bdbcbe221a77597fb81cf6f210a2f751d86e92d
sinsoku <sinsoku.listy@gmail.com>
2019-11-29 19:58:37 +0900

    Add a method to call :to_s in Rust

--- spec/wasabi_spec.rb
+++ spec/wasabi_spec.rb
@@ -8,4 +8,22 @@ RSpec.describe Wasabi do
       expect(Wasabi.sum(1, 2)).to eq 3
     end
   end
+
+  describe '.call_to_s' do
+    context '1.to_s' do
+      subject { Wasabi.call_to_s(1) }
+
+      it { is_expected.to eq '1' }
+    end
+
+    context 'class with :to_s defined' do
+      subject do
+        klass = Class.new
+        klass.define_method(:to_s) { 'foo' }
+        Wasabi.call_to_s(klass.new)
+      end
+
+      it { is_expected.to eq 'foo' }
+    end
+  end
 end
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use ruby_sys::class::{rb_define_module,rb_define_singleton_method};
-use ruby_sys::types::{Value, SignedValue, CallbackPtr};
 use ruby_sys::fixnum;
+use ruby_sys::types::{Value, SignedValue, CallbackPtr};
+use ruby_sys::util::{rb_funcallv, rb_intern};
 use ruru::util::str_to_cstring;
+use std::ptr;
 
 extern fn rb_sum(_mod: Value, a :Value, b: Value) -> Value {
     let a = unsafe { fixnum::rb_num2int(a) as i64 };
@@ -11,10 +13,18 @@ extern fn rb_sum(_mod: Value, a :Value, b: Value) -> Value {
     unsafe { fixnum::rb_int2inum(sum as SignedValue) }
 }
 
+extern fn rb_call_to_s(_mod: Value, obj: Value) -> Value {
+    unsafe {
+        let method_id = rb_intern(str_to_cstring("to_s").as_ptr());
+        rb_funcallv(obj, method_id, 0, ptr::null())
+    }
+}
+
 #[no_mangle]
 pub extern fn Init_wasabi() {
     unsafe {
         let rb_mod = rb_define_module(str_to_cstring("Wasabi").as_ptr());
         rb_define_singleton_method(rb_mod, str_to_cstring("sum").as_ptr(), rb_sum as CallbackPtr, 2);
+        rb_define_singleton_method(rb_mod, str_to_cstring("call_to_s").as_ptr(), rb_call_to_s as CallbackPtr, 1);
     }
 }
