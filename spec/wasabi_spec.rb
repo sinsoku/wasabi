3d278aa1a0a2fc55f1161f2b5ec791e46af82445
sinsoku <sinsoku.listy@gmail.com>
2019-11-29 18:50:40 +0900

    Add a method to calculate sum

--- spec/wasabi_spec.rb
+++ spec/wasabi_spec.rb
@@ -2,4 +2,10 @@ RSpec.describe Wasabi do
   it "has a version number" do
     expect(Wasabi::VERSION).not_to be nil
   end
+
+  describe '.sum' do
+    it "1 + 2 = 3" do
+      expect(Wasabi.sum(1, 2)).to eq 3
+    end
+  end
 end
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,20 @@
-use ruby_sys::class::rb_define_module;
+use ruby_sys::class::{rb_define_module,rb_define_singleton_method};
+use ruby_sys::types::{Value, SignedValue, CallbackPtr};
+use ruby_sys::fixnum;
 use ruru::util::str_to_cstring;
+
+extern fn rb_sum(_mod: Value, a :Value, b: Value) -> Value {
+    let a = unsafe { fixnum::rb_num2int(a) as i64 };
+    let b = unsafe { fixnum::rb_num2int(b) as i64 };
+    let sum = a + b;
+
+    unsafe { fixnum::rb_int2inum(sum as SignedValue) }
+}
 
 #[no_mangle]
 pub extern fn Init_wasabi() {
     unsafe {
-        rb_define_module(str_to_cstring("Wasabi").as_ptr());
+        let rb_mod = rb_define_module(str_to_cstring("Wasabi").as_ptr());
+        rb_define_singleton_method(rb_mod, str_to_cstring("sum").as_ptr(), rb_sum as CallbackPtr, 2);
     }
 }
