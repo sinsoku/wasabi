d7ada5c83e7959f46f45c8f630588179fe223859
sinsoku <sinsoku.listy@gmail.com>
2019-11-30 01:34:57 +0900

    Shorten lines

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
-use ruby_sys::class::{rb_define_module,rb_define_singleton_method};
-use ruby_sys::fixnum;
+use ruby_sys::{class, fixnum, util};
 use ruby_sys::types::{Value, SignedValue, CallbackPtr};
-use ruby_sys::util::{rb_funcallv, rb_intern};
 use ruru::util::str_to_cstring;
 use std::ptr;
 
@@ -15,16 +13,16 @@ extern fn rb_sum(_mod: Value, a :Value, b: Value) -> Value {
 
 extern fn rb_call_to_s(_mod: Value, obj: Value) -> Value {
     unsafe {
-        let method_id = rb_intern(str_to_cstring("to_s").as_ptr());
-        rb_funcallv(obj, method_id, 0, ptr::null())
+        let method_id = util::rb_intern(str_to_cstring("to_s").as_ptr());
+        util::rb_funcallv(obj, method_id, 0, ptr::null())
     }
 }
 
 #[no_mangle]
 pub extern fn Init_wasabi() {
     unsafe {
-        let rb_mod = rb_define_module(str_to_cstring("Wasabi").as_ptr());
-        rb_define_singleton_method(rb_mod, str_to_cstring("sum").as_ptr(), rb_sum as CallbackPtr, 2);
-        rb_define_singleton_method(rb_mod, str_to_cstring("call_to_s").as_ptr(), rb_call_to_s as CallbackPtr, 1);
+        let rb_mod = class::rb_define_module(str_to_cstring("Wasabi").as_ptr());
+        class::rb_define_singleton_method(rb_mod, str_to_cstring("sum").as_ptr(), rb_sum as CallbackPtr, 2);
+        class::rb_define_singleton_method(rb_mod, str_to_cstring("call_to_s").as_ptr(), rb_call_to_s as CallbackPtr, 1);
     }
 }
