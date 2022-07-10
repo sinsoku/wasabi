9c803439a914810a90464eb6d4c04a628f092f76
Takumi Shotoku <sinsoku.listy@gmail.com>
2022-07-10 23:22:42 +0900

    refactor: Generate makefile using by rb_sys

--- Gemfile
+++ Gemfile
@@ -5,4 +5,5 @@ gemspec
 
 gem "rake", "~> 12.0"
 gem "rake-compiler"
+gem "rb_sys"
 gem "rspec", "~> 3.0"
--- ext/wasabi/extconf.rb
+++ ext/wasabi/extconf.rb
@@ -1,31 +1,8 @@
+# frozen_string_literal: true
+
 require "mkmf"
-require "rubygems/ext"
-require "rubygems/ext/cargo_builder"
+require "rb_sys/mkmf"
 
-target = "wasabi"
-dest_path = File.join(Dir.pwd, "target")
-results = []
-args = []
-lib_dir = Dir.pwd
-cargo_dir = File.expand_path("../..", __dir__)
-spec = Struct.new(:name, :metadata).new(target, {})
-
-make_install = <<~EOF
-  target_prefix = /#{target}
-  TARGET = #{target}
-  DLLIB = $(TARGET).#{RbConfig::CONFIG["DLEXT"]}
-  RUBYARCHDIR   = $(sitearchdir)$(target_prefix)
-  CLEANLIBS = release/
-  #{dummy_makefile(__dir__).join("\n").gsub("all install static install-so install-rb", "all static install-so install-rb")}
-  install: $(DLLIB)
-  \t$(INSTALL_PROG) $(DLLIB) $(RUBYARCHDIR)
-EOF
-
-File.write("Makefile", make_install)
-
-begin
-  Gem::Ext::CargoBuilder.new(spec).build(nil, dest_path, results, args, lib_dir, cargo_dir)
-rescue
-  puts results
-  raise
+create_rust_makefile("wasabi/wasabi") do |r|
+  r.ext_dir = '../..'
 end
